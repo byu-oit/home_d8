938b84c3ba07cf88c9de6c10b8919ecca8ca017d
U-BYU\katrialh <katrialh@ITB-DEMO-5520.byu.local>
2017-06-26 15:59:22 -0600

    trying to get home components working, about styling

--- js/homepage.js
+++ js/homepage.js
@@ -2,13 +2,13 @@
  * Created by katrialh on 6/9/2017.
  */
 
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip({trigger: 'manual'}).tooltip('show');
+jQuery(function () {
+    jQuery('[data-toggle="tooltip"]').tooltip({trigger: 'manual'}).tooltip('show');
 });
 
 // $( window ).scroll(function() {
 // if($( window ).scrollTop() > 10){  // scroll down abit and get the action
-$(".progress-bar").each(function(){
-    each_bar_width = $(this).attr('aria-valuenow');
-    $(this).width(each_bar_width + '%');
+jQuery(".progress-bar").each(function(){
+    each_bar_width = jQuery(this).attr('aria-valuenow');
+    jQuery(this).width(each_bar_width + '%');
 });
