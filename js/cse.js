1d3f61f0fbeea9e5793e83f82a9e94d68ecc690e
Sherwin Harris <sherwin@byu.edu>
2017-08-25 16:26:55 -0600

    js and sass for changing the module behavior

---
+++ js/cse.js
@@ -0,0 +1,10 @@
+  (function() {
+    var cx = '009932716493032633443:hlqjz33kfkc';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    gcse.enableAutoComplete=true;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
