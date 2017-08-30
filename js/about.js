23365b3d30691be0034373be03ea81afaa5351a8
Glen Sawyer <github@snelg.com>
2017-08-30 11:43:10 -0600

    Only trigger demographics animation one time

--- js/about.js
+++ js/about.js
@@ -163,9 +163,9 @@
     };
 
     function animateDemographics() {
-        // if (oneTimeAnimateElements.demographics) {
-        //     return;
-        // }
+        if (oneTimeAnimateElements.demographics) {
+            return;
+        }
         if ($('.demographics3').hasClass('in-view')) {  // scroll down abit and get the action
             oneTimeAnimateElements.demographics = true;
 
