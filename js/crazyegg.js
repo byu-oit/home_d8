ee1ca8d426c7c2b38bd4cf652c39d3d28e6c938d
EC2 Default User <ec2-user@ip-10-212-112-59.us-west-2.compute.internal>
2017-09-18 16:20:55 +0000

    add crazyegg.js

---
+++ js/crazyegg.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+setTimeout(function(){var a=document.createElement("script");
+var b=document.getElementsByTagName("script")[0];
+a.src=document.location.protocol+"//script.crazyegg.com/pages/scripts/0053/5042.js";
+a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
+</script>
