3826c8da713680436084fb314a4f2c6aa8b3bf19
Sherwin Harris <sherwin@byu.edu>
2017-10-30 17:57:07 +0000

    invoked function

--- js/survey.js
+++ js/survey.js
@@ -2,22 +2,25 @@
  * Created by katria on 9/1/2017.
  */
 
-$(document).ready(function() {
-    function randomTrigger() {
-// Random number 0 - 2 inclusive
-        var randNum = Math.floor(Math.random() * 3)
-        switch (randNum) {
-            case 0:
-                $trigger=TRUE;
-                break;
-            default:
-                $trigger=FALSE;
-        }
+jQuery(document).ready(function() {
+
+  var trigger=false;
+  function randomTrigger(top) {
+// Random number 0 - top inclusive
+    var randNum = Math.floor(Math.random() * top)
+    switch (randNum) {
+      case 0:
+        trigger=true;
+        break;
+      default:
+        trigger=false;
     }
+  }
     if (typeof(Storage) == 'undefined') {
         return;
     }
-    if($trigger) {
+  randomTrigger(5);
+    if(trigger) {
         if (localStorage.getItem('popState') != 'shown') {
             $("#byu-modal").delay(1000).fadeIn();
             $("#byu-modal").delay(1000).css('display', 'block');
