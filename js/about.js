428e2c4ad94363d9c2827e9c937a8ccae08a22f9
Katria Lesser <katrialynn18@gmail.com>
2018-01-29 13:13:24 -0700

    yfacts adjustments and xlarge jumbo mobile adjstmnts

--- js/about.js
+++ js/about.js
@@ -198,7 +198,7 @@
                 next();
             });
         // should only run for 768px and above:
-        $('.jumbo-text')
+        $('.jumbo-text.and-icon')
             .delay(1200)
             .queue(function (next) {
                 $(this).css('display', 'block');
